0cb8213f0bcd88a6ba59b3f035a5731a2b39c2ae
muzy06038 <sub06038@naver.com>
2021-09-06 16:24:08 +0900

    Forked from https://github.com/creativetimofficial/vue-black-dashboard

---
+++ src/pages/Icons.vue
@@ -0,0 +1,236 @@
+
+<template>
+
+  <div class="card">
+    <card type="chart">
+      <template slot="header">
+        <div class="row">
+          <div class="col-sm-6" :class="isRTL ? 'text-right' : 'text-left'">
+            <h2 class="card-title">{{$t('cctv.title')}}</h2>
+          </div>
+          <div class="col-sm-6">
+            <div class="btn-group btn-group-toggle"
+                  :class="isRTL ? 'float-left' : 'float-right'"
+                  data-toggle="buttons">
+              <label v-for="(option, index) in cctvAreaCategories"
+                      :key="option"
+                      class="btn btn-sm btn-primary btn-simple"
+                      :class="{active: bigLineChart.activeIndex === index}"
+                      :id="index">
+                <input type="radio"
+                        @click="initBigChart(index)"
+                        name="options" autocomplete="off"
+                        :checked="bigLineChart.activeIndex === index">
+                {{option}}
+              </label>
+            </div>
+            <base-button slot="footer" type="primary" fill>Save</base-button>
+          </div>
+        </div>
+      </template>
+      <div class="chart-area">
+        <line-chart style="height: 100%"
+                    ref="bigChart"
+                    chart-id="big-line-chart"
+                    :chart-data="bigLineChart.chartData"
+                    :gradient-colors="bigLineChart.gradientColors"
+                    :gradient-stops="bigLineChart.gradientStops"
+                    :extra-options="bigLineChart.extraOptions">
+        </line-chart>
+      </div>
+    </card>
+    <div class="card-body all-icons">
+      <div class="row">
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class="tim-icons icon-alert-circle-exc"></i>
+            <p>icon-alert-circle-exc</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class="tim-icons icon-align-center"></i>
+            <p>icon-align-center</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class="tim-icons icon-align-left-2"></i>
+            <p>icon-align-left-2</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class="tim-icons icon-app"></i>
+            <p>icon-app</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import LineChart from '@/components/Charts/LineChart';
+  import BarChart from '@/components/Charts/BarChart';
+  import * as chartConfigs from '@/components/Charts/config';
+  import TaskList from './Dashboard/TaskList';
+  import UserTable from './Dashboard/UserTable';
+  import config from '@/config';
+
+  export default {
+    components: {
+      LineChart,
+      BarChart,
+      TaskList,
+      UserTable
+    },
+    data() {
+      return {
+        bigLineChart: {
+          allData: [
+            [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
+            [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
+            [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+          ],
+          activeIndex: 0,
+          chartData: {
+            datasets: [{ }],
+            labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+          },
+          extraOptions: chartConfigs.purpleChartOptions,
+          gradientColors: config.colors.primaryGradient,
+          gradientStops: [1, 0.4, 0],
+          categories: []
+        },
+        purpleLineChart: {
+          extraOptions: chartConfigs.purpleChartOptions,
+          chartData: {
+            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            datasets: [{
+              label: "Data",
+              fill: true,
+              borderColor: config.colors.primary,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.primary,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.primary,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [80, 100, 70, 80, 120, 80],
+            }]
+          },
+          gradientColors: config.colors.primaryGradient,
+          gradientStops: [1, 0.2, 0],
+        },
+        greenLineChart: {
+          extraOptions: chartConfigs.greenChartOptions,
+          chartData: {
+            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+            datasets: [{
+              label: "My First dataset",
+              fill: true,
+              borderColor: config.colors.danger,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.danger,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.danger,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: [90, 27, 60, 12, 80],
+            }]
+          },
+          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
+          gradientStops: [1, 0.4, 0],
+        },
+        blueBarChart: {
+          extraOptions: chartConfigs.barChartOptions,
+          chartData: {
+            labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+            datasets: [{
+              label: "Countries",
+              fill: true,
+              borderColor: config.colors.info,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              data: [53, 20, 10, 80, 100, 45],
+            }]
+          },
+          gradientColors: config.colors.primaryGradient,
+          gradientStops: [1, 0.4, 0],
+        }
+      }
+    },
+    computed: {
+      enableRTL() {
+        return this.$route.query.enableRTL;
+      },
+      isRTL() {
+        return this.$rtl.isRTL;
+      },
+      bigLineChartCategories() {
+        return this.$t('cctv.cctvArea');
+      },
+      cctvAreaCategories(){
+        return this.$t('cctv.cctvArea');
+      }
+    },
+    methods: {
+      initCCTV(index){
+
+      },
+
+      initBigChart(index) {
+        let chartData = {
+          datasets: [{
+            fill: true,
+            borderColor: config.colors.primary,
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: config.colors.primary,
+            pointBorderColor: 'rgba(255,255,255,0)',
+            pointHoverBackgroundColor: config.colors.primary,
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: this.bigLineChart.allData[index]
+          }],
+          labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        }
+        this.$refs.bigChart.updateGradients(chartData);
+        this.bigLineChart.chartData = chartData;
+        this.bigLineChart.activeIndex = index;
+      }
+    },
+    mounted() {
+      this.i18n = this.$i18n;
+      if (this.enableRTL) {
+        this.i18n.locale = 'ar';
+        this.$rtl.enableRTL();
+      }
+      this.initBigChart(0);
+    },
+    beforeDestroy() {
+      if (this.$rtl.isRTL) {
+        this.i18n.locale = 'en';
+        this.$rtl.disableRTL();
+      }
+    }
+  };
+</script>
+<style>
+</style>
